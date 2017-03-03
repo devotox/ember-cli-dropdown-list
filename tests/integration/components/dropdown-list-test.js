e81d8bc66e913e11c1fb64af2b0d6cb274ab5498
Devonte <devo.tox.89@gmail.com>
2017-03-03 22:36:05 +0000

    fix tests

--- tests/integration/components/dropdown-list-test.js
+++ tests/integration/components/dropdown-list-test.js
@@ -9,16 +9,19 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{dropdown-list}}`);
+
+  this.set('content', [{ id: 1, label: 'one' }]);
+
+  this.render(hbs`{{dropdown-list optionValuePath="content.id" optionLabelPath="content.label" }}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#dropdown-list}}
-      template block text
-    {{/dropdown-list}}
+	{{#dropdown-list}}
+	  template block text
+	{{/dropdown-list}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), '');
 });
